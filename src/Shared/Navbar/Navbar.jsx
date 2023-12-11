ac4cacb50ceefbaeec55c5f50e3e8fcd035e7baa
istiakahmedsarker <gamerestiak99@gmail.com>
2023-12-11 12:46:11 +0600

    customized the navbar with font and color

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="bg-[#022835] text-white font-sans font-medium p-4">
-            <ul className="flex space-x-4">
-                <li className="text-white hover:text-teal-300 transition-colors duration-300">
-                    Home
-                </li>
-                <li className="text-white hover:text-teal-300 transition-colors duration-300">
-                    About
-                </li>
-                <li className="text-white hover:text-teal-300 transition-colors duration-300">
-                    Projects
-                </li>
-                <li className="text-white hover:text-teal-300 transition-colors duration-300">
-                    Contact
-                </li>
-            </ul>
+        <nav className="bg-[#022835] text-white font-medium p-4">
+            <div className="flex justify-between items-center">
+                <div className="text-5xl font-bold text-gray-300 transition-colors duration-300">
+                    <h1 >Portfolio</h1>
+                </div>
+                <div className="">
+                    <ul className="flex space-x-4">
+                        <li className="text-gray-300 hover:text-teal-300 transition-colors duration-300">
+                            Home
+                        </li>
+                        <li className="text-gray-300 hover:text-teal-300 transition-colors duration-300">
+                            About
+                        </li>
+                        <li className="text-gray-300 hover:text-teal-300 transition-colors duration-300">
+                            Projects
+                        </li>
+                        <li className="text-gray-300 hover:text-teal-300 transition-colors duration-300">
+                            Contact
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     );
 };
